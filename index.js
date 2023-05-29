ae678232baf7f9f16144c1d14242e3f7212f88a5
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-05-29 12:15:59 +0100

    rewrite

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
