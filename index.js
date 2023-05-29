b8fd935ade5073efda4da0c2c7a30677c83b2944
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-05-29 12:28:12 +0100

    pls post

--- index.js
+++ index.js
@@ -6,7 +6,43 @@ const app = express()
 const port = 1697
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('send api request to get imgs')
+})
+
+app.get("/box/:type/:type2/:boximg", (req, res) => {
+  const params = req.params
+  res.sendFile(`./boxes/${params.type}/${params.type2}/${params.boximg}/`, {
+    root: "/app",
+    //dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  } ,function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("lets go it got sent !!1!111!")
+    }
+  })
+})
+
+app.get("/icon/:game/:sprites/:spritetype/:shinyness/:icon", (req, res) => {
+  const params = req.params
+  res.sendFile(`./icons/${params.game}/${params.sprites}/${params.spritetype}/${params.shinyness}/${params.icon}`, {
+    root: "/app",
+    //dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  } ,function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("lets go it got sent !!1!111!")
+    }
+  })
 })
 
 app.listen(port, () => {
